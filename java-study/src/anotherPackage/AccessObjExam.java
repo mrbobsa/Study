b7f40d2f8818e6ce916e35853e0486c107692b30
minsung <ds3dxf@hanmail.net>
2017-09-05 00:12:16 +0900

    rename, add java study

---
+++ java-study/src/anotherPackage/AccessObjExam.java
@@ -0,0 +1,16 @@
+package anotherPackage;
+
+import javastudy.AccessObj;
+
+public class AccessObjExam extends AccessObj{
+
+	public static void main(String[] args) {
+		AccessObjExam obj = new AccessObjExam();
+		System.out.println(obj.p);
+		System.out.println(obj.p2);
+		System.out.println(obj.k);
+		System.out.println(obj.i);
+		
+	}
+
+}
---
+++ java-study/src/javastudy/AccessObj.java
@@ -0,0 +1,9 @@
+package javastudy;
+
+public class AccessObj {
+	public int p = 3;
+	protected int p2 = 4;
+	int k = 2;
+	private int i = 1;
+	
+}
